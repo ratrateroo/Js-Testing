7d56009bbec69a372facd4916d398954768d46bc
Mark Leo <ratrateroo@gmail.com>
2020-02-07 03:00:57 +0800

    added integration test

--- util.test.js
+++ util.test.js
@@ -1,6 +1,11 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
-test('output name and age', () => {
+test('should output name and age', () => {
     const text = generateText('Mark', 31);
+    expect(text).toBe('Mark (31 years old)');
+});
+
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('Mark', 31);
     expect(text).toBe('Mark (31 years old)');
 });
